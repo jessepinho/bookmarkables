ce7f09b85402dcfb3f2a47e86d41cdb03c1e9cc0
Jesse Pinho <jesse@jessepinho.com>
2017-12-19 21:34:33 +0100

    chore: switch to create-react-app

--- index.js
+++
@@ -1,6 +0,0 @@
-(function() {
-  const fileInput = document.getElementById('file');
-  const reader = new FileReader();
-  fileInput.onchange = () => reader.readAsText(fileInput.files[0]);
-  reader.onload = () => document.getElementById('code').innerHTML = reader.result;
-})();
